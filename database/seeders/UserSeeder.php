4d207acecd9cf374fcf7c6293096e13cca79560e
ujangbedog <domba88galing@gmail.com>
2025-05-03 22:19:57 +0700

    feat: user seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@xpqx.xyz',
+            'password' => Hash::make('password'),
+        ]);
+    }
+}
